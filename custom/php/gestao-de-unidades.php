8ffe2cb3b0aaddbdf9565d5aedab1a8c5d2e1962
Mistakx <ade9@live.com.pt>
2021-12-08 13:12:49 -0500

    Added subtypes to table

--- custom/php/gestao-de-unidades.php
+++ custom/php/gestao-de-unidades.php
@@ -11,16 +11,14 @@ printCurrentUserRoles($currentUser);
 
 //* Database information
 $databaseConnection = connectToDatabase();
-$query = "SELECT * FROM subitem_unit_type";
-// $query = "SELECT * FROM child";
-$queryResult = $databaseConnection->query($query);
+$subitemTypes = $databaseConnection->query("SELECT * FROM subitem_unit_type");
 
 // echo "\nQuery result:\n";
-// print_r($queryResult);
+// print_r($subitemTypes);
 echo "\n";
 
 //* If there are no unit types in the database
-if ($queryResult->num_rows == 0) {
+if ($subitemTypes->num_rows == 0) {
     
     echo "<strong> There are no unit types.</strong>";
 
@@ -38,14 +36,28 @@ if ($queryResult->num_rows == 0) {
 
     // For each subitem type table row: kg, cm
     // Subitem type: id, name 
-    foreach($queryResult->fetch_all() as $queryArray) {
+    foreach($subitemTypes->fetch_all() as $subitemType) {
 
-        echo "<tr>";
+        echo "<tr>"; // Begin table row
+
+        echo "<td>" . $subitemType[0] . "</td>"; // First column: id
+        echo "<td>" . $subitemType[1] . "</td>"; // Second column: name
+
+        echo "<td>"; // Third column: nomes subitens que têm respetivo tipo de unidade, aparecendo dentro de parêntesis o nome do item a que pertence esse subitem
+
+        // Query the subitems that have the same subitem type of this foreach loop instance subitem type
+        $subitems = $databaseConnection->query("SELECT * FROM subitem WHERE subitem.unit_type_id = " . $subitemType[0]);
+        // print_r($subitems);
+
+        // For each subitem table row that has the same subitem type of this foreach loop instance subitem type
+        foreach($subitems as $subitem) {
+            print_r($subitem);
+            echo $subitem["name"] . ", "; // name
+        }
+
+        echo "</td>";
         
-        echo "<td>" . $queryArray[0] . "</td>"; // id
-        echo "<td>" . $queryArray[1] . "</td>"; // name
-
-        echo "</tr>";
+        echo "</tr>"; // End table row
 
     }
 
