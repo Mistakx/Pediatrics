6d6cb8376374356d8b50546a6bb04b37e750866c
Mistakx <ade9@live.com.pt>
2021-12-10 12:14:39 -0500

    Mange unit types form complete

--- custom/php/gestao-de-unidades.php
+++ custom/php/gestao-de-unidades.php
@@ -1,31 +1,50 @@
 <?php 
+// kU3o7LHl8HKq
 
 require_once("custom/php/common.php");
 
-verifyLoginAndCapability("manage_unit_types");
+function handle_request($databaseConnection) {
 
-//* User information
-$currentUser = wp_get_current_user();
-printCurrentUsername($currentUser);
-printCurrentUserRoles($currentUser);
+    if ( array_key_exists("Estado", $_REQUEST) and $_REQUEST['Estado'] == "Inserir") { //* If the user has inserted some unit type in the database
 
-//* Database information
-$databaseConnection = connectToDatabase();
-$subitemTypes = $databaseConnection->query("SELECT * FROM subitem_unit_type");
+        if ( array_key_exists("Nome", $_REQUEST) and $_REQUEST['Nome'] != "") { //* Valid unit type name
 
-// echo "\nQuery result:\n";
-// print_r($subitemTypes);
-echo "\n";
+            $unitInserted = $_REQUEST['Nome'];
+            $unitNamesInDatabase = $databaseConnection->query("SELECT name FROM subitem_unit_type");
+            $unitNameAlreadyExists = FALSE;
+            foreach ($unitNamesInDatabase as $unitNameInDatabase) { // Check if unit already exists in the database                
+                if ($unitInserted == $unitNameInDatabase["name"]) {
+                    $unitNameAlreadyExists = TRUE;
+                    break;
+                }
+            }
+            
+            if ($unitNameAlreadyExists) {
+                echo "<script>alert('O valor $unitInserted já existe na base de dados.')</script>";
+            } else {
+                $databaseConnection->query("INSERT INTO subitem_unit_type (name) VALUES ('$unitInserted')");
+                // echo "<script>alert('Foi inserido o valor $unitInserted')</script>";
+            }
 
-//* If there are no unit types in the database
-if ($subitemTypes->num_rows == 0) {
+
+
+        } else { //* Invalid unit type name
+            echo "O nome da unidade enviada foi inválido";
+            echo "<br><br>";
+        }
     
-    echo "<strong> There are no unit types.</strong>";
+    } else { //* If the user entered the page as usual, without sending any unit type to the database
+        return;
+    }
 
-} else { //* If there are unit types in the database
+}
 
-    // Table beginning
-    echo "<table>";
+function unit_types_table($databaseConnection) {
+
+    $subitemTypes = $databaseConnection->query("SELECT * FROM subitem_unit_type");
+
+
+    echo "<table>"; // Table beginning
 
     // Table header
     echo "<tr>
@@ -65,12 +84,49 @@ if ($subitemTypes->num_rows == 0) {
 
     }
 
-    // Table ending
-    echo "</table>"; 
-
-
+    echo "</table>"; // Table ending  
 }
 
+function insert_unit_type_form() {
+
+    echo "<h3>Gestão de unidades - introdução</hr>"; 
+
+    echo "<form method='post'>"; // Form beginning
+        echo "<input type='text' name='Nome' >";
+        echo "<input type='hidden' name='Estado' value='Inserir'>";
+        echo "<button> Submeter </button>";
+    echo "</form>"; // Form ending
+    
+}
+
+echo "DEBUG";
+
+//* Verify if the user is logged in, and if it has the manage_unit_types capability
+verifyLoginAndCapability("manage_unit_types");
+
+//* User information
+$currentUser = wp_get_current_user();
+printCurrentUsername($currentUser);
+printCurrentUserRoles($currentUser);
+
+echo "<br>";
+
+//* Database information
+$databaseConnection = connectToDatabase();
+$subitemTypes = $databaseConnection->query("SELECT * FROM subitem_unit_type");
+
+handle_request($databaseConnection);
+
+if ($subitemTypes->num_rows == 0) { //* If there are no unit types in the database
+     
+    echo "<strong> There are no unit types.</strong>";
+    
+} else { //* If there are unit types in the database
+
+    unit_types_table($databaseConnection, $subitemTypes);
+    insert_unit_type_form();
+
+}
 
 
 ?> 
