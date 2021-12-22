986f5e488a6ce37b363843f95956168cb83636c9
Mistakx <ade9@live.com.pt>
2021-12-22 09:44:17 -0500

    Completed allowed values page form

--- custom/php/gestao-de-unidades.php
+++ custom/php/gestao-de-unidades.php
@@ -5,107 +5,182 @@ require_once("custom/php/common.php");
 
 function handle_request($databaseConnection) {
 
-    if ( array_key_exists("Estado", $_REQUEST) and $_REQUEST['Estado'] == "Inserir") { //* User has inserted some unit type
+    if ( array_key_exists("Estado", $_REQUEST) and $_REQUEST['Estado'] == "Introducao") { //* User has clicked a subitem
+      
+        $_REQUEST["Subitem_id"] = $_REQUEST["Subitem"];
+        $subitemID = $_REQUEST['Subitem_id']; 
+        return array(true, $subitemID); 
 
-        if ( array_key_exists("Nome", $_REQUEST) and $_REQUEST['Nome'] != "") { //* Non empty unit type name
+    } else if ( array_key_exists("Estado", $_REQUEST) and $_REQUEST['Estado'] == "Inserir") { //* User has inserted some value
+       
+        $_REQUEST["Subitem_id"] = $_REQUEST["Subitem"];
+        $subitemID = $_REQUEST['Subitem_id']; 
+        $valueToInsert = $_REQUEST['Valor']; 
 
-            $unitInserted = $_REQUEST['Nome'];
-            $unitNamesInDatabase = $databaseConnection->query("SELECT name FROM subitem_unit_type"); // TODO: Not necessary, query was executed before on the main function, and can be sent as parameter
-            $unitNameAlreadyExists = FALSE;
-            foreach ($unitNamesInDatabase as $unitNameInDatabase) { // Check if unit already exists in the database                
-                if ($unitInserted == $unitNameInDatabase["name"]) {
-                    $unitNameAlreadyExists = TRUE;
+        if ( $valueToInsert != "" ) { //* Non empty value
+
+            $valuesInDatabase = $databaseConnection->query("SELECT value FROM subitem_allowed_value WHERE subitem_allowed_value.subitem_id = $subitemID");
+            $valueAlreadyExists = FALSE;
+            foreach ($valuesInDatabase as $valueInDatabase) { // Check if unit already exists in the database                
+                if ($valueToInsert == $valueInDatabase["value"]) {
+                    $valueAlreadyExists = TRUE;
                     break;
                 }
             }
             
-            if ($unitNameAlreadyExists) {
-                echo "<script>alert('O valor $unitInserted já existe na base de dados.')</script>";
+            if ($valueAlreadyExists) {
+                echo "<script>alert('O valor $valueToInsert já existe na base de dados.')</script>";
             } else {
-                $databaseConnection->query("INSERT INTO subitem_unit_type (name) VALUES ('$unitInserted')");
-                $alertText = "alert('Foi inserido o valor $unitInserted.')";
-                // $alertText = "<script> if (window.confirm('If you click ok you would be redirected . Cancel will load this website ')) { window.location.href='https://www.google.com/chrome/browser/index.html'; };";
-                // $alertText = "window.postMessage('Foi inserido o valor $unitInserted.')";
-                // $alertText.= "<a href='gestao-de-unidades.php'>Continuar</a>";
-                echo "<script>$alertText</script>";
+                $databaseConnection->query("INSERT INTO subitem_allowed_value (subitem_id, value) VALUES ('$subitemID', '$valueToInsert')");
+                $alertText = "alert('Foi inserido o valor $valueToInsert.')";
+                echo "<script>$alertText</script>)";
+
             }
 
+        } else { //* Empty value
 
-            
+            echo "<script>alert('O nome do valor permitido enviado foi inválido.')</script>";
 
-
-        } else { //* Invalid unit type name
-            echo "<script>alert('O nome da unidade enviada foi inválido.')</script>";
-            echo "<br><br>";
         }
-    
-    } else { //* If the user entered the page as usual, without inserting any unit type
-        return;
+
+    } else { //* If the user entered the page as usual
+        return array(false, NULL);
     }
 
 }
 
-function unit_types_table($databaseConnection) {
+function allowed_values_table($databaseConnection) {
 
-    $subitemTypes = $databaseConnection->query("SELECT * FROM subitem_unit_type");
-
-    echo "<table>"; // Table beginning
-
-    // Table header
-    echo "<tr>
-            <th> <strong> id </strong> </th>
-            <th> <strong> unidade </strong> </th>
+    echo "<table>"; //* Table beginning
+    
+    //* Table header
+    echo "<tr> 
+            <th> <strong> item </strong> </th>
+            <th>id</th>
             <th> <strong> subitem </strong> </th>
-        </tr>";
+            <th>id</th>
+            <th>valores permitidos</th>
+            <th>estado</th>
+            <th>ação</th>
+    </tr>";
 
-    //* For each subitem type
-    foreach($subitemTypes as $subitemType) {
+    //! Query all item names and IDs
+    $itemsNamesAndIDs = $databaseConnection->query("SELECT 
+    item.name as itemName, 
+    item.id as itemID
+    FROM item");
+    foreach ($itemsNamesAndIDs as $itemNameAndID) {
+        
+        $itemRowSpan = 0;
+        $itemRow = "";
 
+        $itemRow.="<tr>";
 
-        echo "<tr>"; // Begin table row
+        // Item name
+        $itemRow.="<td rowspan=mistakxItemSpan> $itemNameAndID[itemName] </td>";
 
-        echo "<td>" . $subitemType["id"] . "</td>"; //* First column
-        echo "<td>" . $subitemType["name"] . "</td>"; //* Second column
+        //! Query all associated subitem names and IDs
+        $subitemsNamesAndIDs = $databaseConnection->query("SELECT 
+        subitem.name as subitemName,
+        subitem.id as subitemID
+        FROM subitem
+        WHERE subitem.item_id = $itemNameAndID[itemID]");
+        // print_r("SUBITEMS OF $itemNameAndID[itemName]:\n");
+        // print_r($subitemsNamesAndIDs);
+        foreach($subitemsNamesAndIDs as $subitemNameAndID) {
 
-        echo "<td>"; // Third column: nomes subitens que têm respetivo tipo de unidade, aparecendo dentro de parêntesis o nome do item a que pertence esse subitem
+            $subitemRow = "";
+            $subitemRowSpan = 0; // Number of allowed values the subitem has
 
-        // Query all the "subitems" that have the same "subitem type" of this loop "subitem type"
-        $subitems = $databaseConnection->query("SELECT * FROM subitem WHERE subitem.unit_type_id = " . $subitemType["id"]);
+            if ($subitemRowSpan != 0) {$subitemRow.="<tr>";} // First element continues in the same line, after that it's a new row               
 
-        //* For each "subitem" that has the same "subitem type" of this loop "subitem type"
-        foreach($subitems as $subitem) {
+            // Subitem ID
+            $subitemRow.="<td rowspan=mistakxSubitemSpan> $subitemNameAndID[subitemID] </td>";
 
-            echo $subitem["name"];
+            // Subitem name
+            $subitemRow.="<td rowspan=mistakxSubitemSpan> <a href='?Estado=Introducao&Subitem=$subitemNameAndID[subitemID]'>$subitemNameAndID[subitemName]</a> </td>";
 
-            // Query the "item" parent of the loop "subtitem"
-            $item = $databaseConnection->query("SELECT * FROM item WHERE item.id = " . $subitem["item_id"]);
-            echo " (". $item->fetch_assoc()["name"] . "), ";
+            //! Query all associated allowed values and IDs
+            $allowedValuesAndIDs = $databaseConnection->query("SELECT 
+            subitem_allowed_value.id as allowedValueID, 
+            subitem_allowed_value.value as allowedValue, 
+            subitem_allowed_value.state as allowedValueState
+            FROM subitem_allowed_value 
+            WHERE subitem_allowed_value.subitem_id = $subitemNameAndID[subitemID]");
+            // print_r("ALLOWED VALUES:\n");
+            // print_r($allowedValuesAndIDs);
+            foreach($allowedValuesAndIDs as $allowedValueAndID) {
+
+                $allowedValueRow = "";
+
+                if ($subitemRowSpan != 0) {$allowedValueRow.="<tr>";} // First element continues in the same line, after that it's a new row               
+
+                // Allowed value ID
+                $allowedValueRow.="<td> $allowedValueAndID[allowedValueID] </td>";
+
+                // Allowed value
+                $allowedValueRow.="<td> $allowedValueAndID[allowedValue] </td>";
+        
+                // Allowed value state
+                if ($allowedValueAndID["allowedValueState"] == "active") {
+                    $allowedValueRow.="<td> ativo </td>";
+                } else {
+                   $allowedValueRow.="<td> inativo </td>";
+                }
+
+                // Action
+                $allowedValueRow.="<td>";
+                $allowedValueRow.="[editar]<br>";
+                if ($allowedValueAndID["allowedValueState"] == "active") {
+                    $allowedValueRow.="[desativar]";
+                } else {
+                    $allowedValueRow.="[ativar]";
+                }
+                $allowedValueRow.="</td>";
+                
+                if ($subitemRowSpan != 0) {$allowedValueRow.="</tr>";} // First element continues in the same line, after that it's a new row 
+                $subitemRow.=$allowedValueRow;
+                $subitemRowSpan++;
+
+            }
+
+            if ($subitemRowSpan == 0) { // If subitem doesn't have any allowed values
+                $subitemRow.="<td colspan=4>Não há valores predefinidos (subitem)</td>"; // TODO: Make this more dynamic
+                $subitemRowSpan++; // The subitem row span can't be 0 because it bugs itself, so make it 1
+            }
+            if ($subitemRowSpan != 0) {$subitemRow.="</tr>";} // First element continues in the same line, after that it's a new row               
+            $itemRowSpan = $itemRowSpan + $subitemRowSpan;
+            $itemRow.= str_replace("mistakxSubitemSpan", strval($subitemRowSpan),$subitemRow);
 
         }
 
-        echo "</td>";
         
-        echo "</tr>"; // End table row
+        if ($itemRowSpan == 0) { // If item doesn't have any subitems
+            $itemRow.="<td colspan=6>Não há valores predefinidos (item)</td>"; // TODO: Make this more dynamic
+            $itemRowSpan++; // The item row span can't be 0 because it bugs itself, so make it 1
+        } 
+        $itemRow.="</tr>"; 
+        echo str_replace("mistakxItemSpan", strval($itemRowSpan),$itemRow);
 
     }
 
     echo "</table>"; // Table ending  
 }
 
-function insert_unit_type_form() {
-
-    echo "<h3>Gestão de unidades - introdução</h3>"; 
+function allowed_values_form($subitemID) {
+    echo "<h3>Gestão de valores permitidos - introdução</h3>"; 
 
     echo "<form method='post'>"; // Form beginning
-        echo "<input type='text' name='Nome' placeholder='Unidade a inserir'>";
+        echo "<input type='text' name='Valor' placeholder='Subitem ID: $subitemID' >";
         echo "<input type='hidden' name='Estado' value='Inserir'>";
         echo "<button> Submeter </button>";
     echo "</form>"; // Form ending
-    
 }
 
+print_r($_REQUEST);
+
 //* Verify if the user is logged in, and if it has the manage_unit_types capability
-verifyLoginAndCapability("manage_unit_types");
+verifyLoginAndCapability("manage_allowed_values");
 
 //* User information
 $currentUser = wp_get_current_user();
@@ -116,19 +191,17 @@ echo "<br>";
 
 //* Database information
 $databaseConnection = connectToDatabase();
-$subitemTypes = $databaseConnection->query("SELECT * FROM subitem_unit_type");
+$items = $databaseConnection->query("SELECT * FROM item");
 
-handle_request($databaseConnection);
 
-if ($subitemTypes->num_rows == 0) { //* If there are no unit types in the database
+if ($items->num_rows == 0) { //* If there are no subitems in the database
      
-    echo "<strong> Não há tipos de unidades.</strong>";
+    echo "<strong> Não há subitens especificados.</strong>";
     
-} else { //* If there are unit types in the database
-
-    unit_types_table($databaseConnection, $subitemTypes);
-    insert_unit_type_form();
-
+} else { //* If there are subitems in the database
+    list($showForm, $subitemID) = handle_request($databaseConnection);
+    allowed_values_table($databaseConnection);
+    if ($showForm) { allowed_values_form($subitemID);}
 }
 
 
